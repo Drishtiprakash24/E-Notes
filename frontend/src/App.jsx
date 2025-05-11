e848ec6c6e9ac395d01a84318ecfecc302c8f013
Drishti Prakash <drishtiprakash24@gmail.com>
2025-05-11 12:16:44 +0530

    Fix routes and redirects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 const routes = (
   <Router>
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/dashboard" exact element={<Home />} />
-      <Route path="/Login" exact element={<Login />} />
-      <Route path="/SignUp" exact element={<SignUp />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/signup" exact element={<SignUp />} />
     </Routes>
   </Router>
 );
